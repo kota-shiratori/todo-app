8467c7270b80db47df440739d8f7ddc08825c48d
shiratori <shiratori@elplanning.co.jp>
2025-02-26 10:46:49 +0900

    jsxもサポート対象

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
@@ -21,6 +21,9 @@ module.exports = {
                 },
             },
         ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
     },
     plugins: [
         new HtmlWebpackPlugin({
